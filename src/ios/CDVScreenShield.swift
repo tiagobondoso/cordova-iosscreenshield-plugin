f6e57d794f43a2e7b089a76e20908bbb0b45135d
tiagobondoso <77240470+tiagobondoso@users.noreply.github.com>
2024-11-05 17:52:42 +0000

    Code and version update

--- src/ios/CDVScreenShield.swift
+++ src/ios/CDVScreenShield.swift
@@ -11,13 +11,20 @@ import WebKit
 
 @objc(CDVScreenShield)
 class CDVScreenShield: CDVPlugin {
-    
+    private var secureView: SecureView? // Armazenando como variável de instância
+
     @objc(protectWebView:)
     func protectWebView(command: CDVInvokedUrlCommand) {
         DispatchQueue.main.async {
             let shouldBlockScreenRecording = (command.arguments.first as? Bool) ?? false
+            
             if let webView = self.webView as? WKWebView {
-                guard let secureView = SecureField().secureContainer else { return }
+                // Verifica se secureView já existe, senão cria um novo
+                if self.secureView == nil {
+                    self.secureView = SecureField().secureContainer
+                }
+
+                guard let secureView = self.secureView else { return }
                 
                 // Add WKWebView to the secure container
                 secureView.addSubview(webView)
@@ -26,13 +33,14 @@ class CDVScreenShield: CDVPlugin {
                 // Add the secure container to the main Cordova view
                 if let cordovaViewController = self.viewController {
                     cordovaViewController.view.addSubview(secureView)
-                    //secureView.pinEdges()
                     secureView.pinEdges(to: cordovaViewController.view)
                 }
                 
                 // Activate screen recording protection (video)
                 if shouldBlockScreenRecording {
-                    guard let message = command.arguments[1] as? String, let fontSize = command.arguments[2] as? CGFloat, let fontColor = command.arguments[3] as? String else {
+                    guard let message = command.arguments[1] as? String,
+                          let fontSize = command.arguments[2] as? CGFloat,
+                          let fontColor = command.arguments[3] as? String else {
                         let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Missing input parameters")
                         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
                         return
@@ -49,34 +57,28 @@ class CDVScreenShield: CDVPlugin {
         }
     }
 
-
     @objc(removeProtectionFromWebView:)
     func removeProtectionFromWebView(command: CDVInvokedUrlCommand) {
         DispatchQueue.main.async {
-            // Verifica se self.webView não é nil e se tem um superview
             guard let webView = self.webView else {
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "WebView is nil")
                 self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
                 return
             }
 
-            guard let secureView = SecureField().secureContainer else { return }
-            
-            // Verifica se o superview é do tipo SecureView
-            if secureView {
-                // Remove WKWebView do container seguro e adiciona de volta à visão principal do Cordova
-                if let cordovaViewController = self.viewController {
-                    secureView.removeFromSuperview()
-                    cordovaViewController.view.addSubview(webView as! UIView)
-                    (webView as! UIView).pinEdges(to: cordovaViewController.view)
-                } else {
-                    let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "No ViewController found")
-                    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                    return
-                }
+            guard let secureView = self.secureView else {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "SecureView is nil")
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                return
+            }
+
+            // Remove WKWebView do container seguro e adiciona de volta à visão principal do Cordova
+            if let cordovaViewController = self.viewController {
+                secureView.removeFromSuperview()
+                cordovaViewController.view.addSubview(webView as! UIView)
+                (webView as! UIView).pinEdges(to: cordovaViewController.view)
             } else {
-                let message = secureView is SecureView ? "WebView is not within a secure container" : "WebView does not have a superview"
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: message)
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "No ViewController found")
                 self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
                 return
             }
@@ -89,3 +91,4 @@ class CDVScreenShield: CDVPlugin {
         }
     }
 }
+
