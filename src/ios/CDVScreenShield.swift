327eb2b099290e0e9d01d9dcf02f1ca057c843ed
tiagobondoso <77240470+tiagobondoso@users.noreply.github.com>
2024-11-08 15:12:05 +0000

    V1.6

--- src/ios/CDVScreenShield.swift
+++ src/ios/CDVScreenShield.swift
@@ -9,25 +9,25 @@ import Foundation
 import UIKit
 import WebKit
 
+@objc(CDVScreenShield)
 class CDVScreenShield: CDVPlugin {
-    private var secureView: UIView? // Variável de instância global
-
+    
     @objc(protectWebView:)
     func protectWebView(command: CDVInvokedUrlCommand) {
         DispatchQueue.main.async {
             let shouldBlockScreenRecording = (command.arguments.first as? Bool) ?? false
             if let webView = self.webView as? WKWebView {
-                self.secureView = SecureField().secureContainer else { return }
+                guard let secureView = SecureField().secureContainer else { return }
                 
                 // Add WKWebView to the secure container
-                self.secureView.addSubview(webView)
-                webView.pinEdges(to: self.secureView)
+                secureView.addSubview(webView)
+                webView.pinEdges(to: secureView)
                 
                 // Add the secure container to the main Cordova view
                 if let cordovaViewController = self.viewController {
-                    cordovaViewController.view.addSubview(self.secureView)
+                    cordovaViewController.view.addSubview(secureView)
                     //secureView.pinEdges()
-                    self.secureView.pinEdges(to: cordovaViewController.view)
+                    secureView.pinEdges(to: cordovaViewController.view)
                 }
                 
                 // Activate screen recording protection (video)
@@ -46,33 +46,6 @@ class CDVScreenShield: CDVPlugin {
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "No WKWebView found")
                 self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
             }
-        }
-    }
-
-    @objc(removeProtectionFromWebView:)
-    func removeProtectionFromWebView(command: CDVInvokedUrlCommand) {
-        DispatchQueue.main.async {
-            guard let secureView = self.secureView else {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "No secure container found")
-                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                return
-            }
-
-            secureView.removeFromSuperview()
-
-            if let cordovaViewController = self.viewController, let webView = self.webView as? UIView {
-                cordovaViewController.view.addSubview(webView)
-                webView.pinEdges(to: cordovaViewController.view)
-            } else {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "No ViewController found")
-                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                return
-            }
-
-            ScreenShield.shared.deactivateProtection()
-
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
     }
 }
